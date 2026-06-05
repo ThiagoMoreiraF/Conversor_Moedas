41b53ce6774d9d86093a8ad10385f56dd1da7669
Thiago Moreira <thiagomf2@gmail.com>
2026-06-05 11:29:25 -0300

    Chamada do script e primeiras configuraçoes no botão.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+const convertButton = document.querySelector(".convert-button");
+
+function convertValues(){
+    console.log("funciona porra")
+}
+
+convertButton.addEventListener("click",convertValues);
